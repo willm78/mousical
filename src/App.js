f9aee16b556c1ce87d0cdbb1f163406e881c2a6d
Will <will.mcdaniell@tamu.edu>
2019-02-01 19:52:58 -0600

    add basic functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
+import Tone from 'tone';
 
 import './App.css';
 
+const synth = new Tone.AMSynth().toMaster();
+
 class App extends Component {
+  state = {
+    on: false,
+    x: 0,
+    y: 0,
+  };
+
+  toggleOnOff = () => {
+    this.setState(({ on }) => {
+      if (!on) {
+        const result = window.confirm('This app makes noise! Is this ok?');
+        return result ? { on: true } : null;
+      }
+      return { on: false };
+    });
+  };
+
+  onMouseMove = ({ clientX: x, clientY: y }) => {
+    synth.triggerAttackRelease(x, '8n');
+    this.setState({ x, y });
+  };
+
   render() {
-    return <div>Mousica</div>;
+    const { on, x, y } = this.state;
+    return (
+      <div className="App" onMouseMove={on ? this.onMouseMove : () => null}>
+        <h1>Mousical</h1>
+        <button onClick={this.toggleOnOff} className="StartBtn">
+          {on ? 'Stop' : 'Start'}
+        </button>
+        <div
+          style={{ visibility: on ? 'visible' : 'hidden' }}
+        >{`Frequency: ${x}Hz`}</div>
+      </div>
+    );
   }
 }
 
